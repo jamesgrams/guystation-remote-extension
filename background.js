bcbb952f96ba703fb413580e01a1a1fc68a0b466
James Grams <james@game103.net>
2020-07-02 09:30:59 -0400

    Add delay between script line execution

--- background.js
+++ background.js
@@ -4,9 +4,16 @@ chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
     chrome.tabs.create({
         url: request.url
     }, function( tab ) {
-        chrome.tabs.executeScript(tab.id, {
-            "code": request.code
-        });
+        if( request.script ) {
+            let script = request.script;
+            script.unshift(""); // this will ensure a delay at the start
+            script = script.join("; await new Promise(resolve => setTimeout(resolve, 3000));")
+            script = "(async function() {" + script + "})();";
+            console.log(script);
+            chrome.tabs.executeScript(tab.id, {
+                "code": script
+            });
+        }
     });
 
     sendResponse();
--- content.js
+++ content.js
@@ -3,14 +3,14 @@
  * This needs to be called within the content script to work properly.
  * externally_connectable doesn't seem to work on local ips.
  * @param {string} event.detail.url - The url to run the script on.
- * @param {string} event.detail.code - The JavaScript code to execute.
+ * @param {Array<string>} event.detail.script - The JavaScript script to execute. Each entry contains a line to be run 3 seconds after one another.
  */
 function gsExtensionRunScript( event ) {
-    chrome.runtime.sendMessage({url: event.detail.url, code: event.detail.code});
+    chrome.runtime.sendMessage({url: event.detail.url, script: event.detail.script});
 }
 window.addEventListener('gsExtensionRunScript', gsExtensionRunScript);
 
 // Inject the function on the page for use.
 var script = document.createElement("script");
-script.textContent = "var gsExtensionRunScript = function(url, code) { window.dispatchEvent( new CustomEvent('gsExtensionRunScript', { detail: {url:url,code:code} }) ); }";
+script.textContent = "var gsExtensionRunScript = function(url, script) { window.dispatchEvent( new CustomEvent('gsExtensionRunScript', { detail: {url:url,script:script} }) ); }";
 document.body.appendChild(script);
